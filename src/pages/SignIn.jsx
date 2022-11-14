e9ca249e64032281521540d0cfeda847935fa4b3
suman osti <suman.osti401@gmail.com>
2022-11-14 09:12:46 +0545

    design done for sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default SignIn
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-0">Sign In</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-4xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="key"
+            className="rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "15px",
+              marginBottom: "15px",
+            }}
+          >
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            />
+            <div className="relative">
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              )}
+            </div>
+            <div className="flex justify-between">
+              <p style={{ fontSize: "12px", margin: "5px" }}>
+                Dont't have a account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out"
+                >
+                  &nbsp;Register
+                </Link>
+              </p>
+              <p style={{ fontSize: "12px", margin: "5px" }}>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-700 duration-200 transition ease-in-out"
+                >
+                  Forgot Password?
+                </Link>
+              </p>
+            </div>
+            <button
+              className="w-full bg-blue-600 text-white px-7 py-3 rounded font-medium uppercase text-sm shadow-sm hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              type="submit"
+            >
+              Sign in
+            </button>
+            <div
+              className="my-4 items-center before:border-t flex before:flex-1 before:border-gray-300
+          after:border-t after:flex-1 after:border-gray-300"
+            >
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
