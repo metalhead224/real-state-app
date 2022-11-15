0bbfa06983449de463470f4bfd63971946327fd7
suman osti <suman.osti401@gmail.com>
2022-11-15 11:48:25 +0545

    header made dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    })
+  }, [auth])
 
   function pathMatchRoute(route) {
     if (route === location.pathname) {
@@ -42,11 +55,11 @@ const Header = () => {
             </li>
             <li
               className={`py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent cursor-pointer ${
-                pathMatchRoute("/sign-in") && "text-gray-900 border-b-red-500"
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-gray-900 border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign in
+              {pageState}
             </li>
           </ul>
         </div>
